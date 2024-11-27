8f90d68be4795b4a6b3f661e4afa8939a529c042
Jungnahyeon <nahyun4669@gmail.com>
2024-11-27 21:43:57 +0900

    Delete netlify/functions directory

--- netlify/functions/getHandler.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Hello, ${name}!` }),
-  };
-};
--- netlify/functions/postHandler.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  const data = JSON.parse(event.body);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Hello, ${data.name || "World"}!` }),
-  };
-};
