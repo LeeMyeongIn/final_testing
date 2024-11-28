afbc3952eeb81b2f283891224640aab2a91e2f5b
Jungnahyeon <nahyun4669@gmail.com>
2024-11-28 18:30:31 +0900

    Delete assets directory

--- assets/js/navigation.js
+++
@@ -1,8 +0,0 @@
-function navigateToPage(page) {
-    console.log(`Navigating to: ${page}`);
-    window.location.href = page; // 지정된 페이지로 이동
-}
-
-function navigateToHome() {
-    window.location.href = "index.html"; // 홈으로 이동
-}
