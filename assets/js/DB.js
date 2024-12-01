89ac818dff697b4c5b97a34538d31d42e1038581
Jungnahyeon <nahyun4669@gmail.com>
2024-12-01 23:32:38 +0900

    Update DB.js

--- assets/js/DB.js
+++ assets/js/DB.js
@@ -1,8 +1,6 @@
-// Firebase 구성 정보
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue } from "firebase/database";
-import { getAuth } from "firebase/auth";
+// DB.js 파일 수정
 
+// Firebase 구성 정보
 const firebaseConfig = {
     apiKey: "AIzaSyAHSA0O4V9vnc5E1YvQNpQTZT5z4QOWE1k",
     authDomain: "software-89c07.firebaseapp.com",
@@ -15,16 +13,15 @@ const firebaseConfig = {
 };
 
 // Firebase 초기화
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 // Firebase 연결 확인
-const connectedRef = ref(database, '.info/connected');
-onValue(connectedRef, (snapshot) => {
+const connectedRef = database.ref('.info/connected');
+connectedRef.on('value', (snapshot) => {
     if (snapshot.val() === true) {
         console.log('Firebase에 연결되었습니다.');
     } else {
         console.log('Firebase 연결이 끊어졌습니다.');
     }
 });
-
