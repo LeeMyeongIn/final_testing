a2994be180cee3da66f34fd7d56d0a2dc8edace5
Jungnahyeon <nahyun4669@gmail.com>
2024-12-01 23:18:43 +0900

    Update DB.js

--- assets/js/DB.js
+++ assets/js/DB.js
@@ -1,22 +1,30 @@
-        // Firebase 구성 정보
-        const firebaseConfig = {
-            apiKey: "AIzaSyAHSA0O4V9vnc5E1YvQNpQTZT5z4QOWE1k",
-            authDomain: "software-89c07.firebaseapp.com",
-            databaseURL: "https://software-89c07-default-rtdb.firebaseio.com",
-            projectId: "software-89c07",
-            storageBucket: "software-89c07.appspot.com",
-            messagingSenderId: "567506244346",
-            appId: "1:567506244346:web:a22bba69dd21ae59146f43",
-            measurementId: "G-KQT14W936Y"
-        };
-        // Firebase 초기화
-        firebase.initializeApp(firebaseConfig);
-        const database = firebase.database();
-        // Firebase 연결 확인
-        firebase.database().ref('.info/connected').on('value', (snapshot) => {
-            if (snapshot.val() === true) {
-                console.log('Firebase에 연결되었습니다.');
-            } else {
-                console.log('Firebase 연결이 끊어졌습니다.');
-            }
-        });
+// Firebase 구성 정보
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHSA0O4V9vnc5E1YvQNpQTZT5z4QOWE1k",
+    authDomain: "software-89c07.firebaseapp.com",
+    databaseURL: "https://software-89c07-default-rtdb.firebaseio.com",
+    projectId: "software-89c07",
+    storageBucket: "software-89c07.appspot.com",
+    messagingSenderId: "567506244346",
+    appId: "1:567506244346:web:a22bba69dd21ae59146f43",
+    measurementId: "G-KQT14W936Y"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Firebase 연결 확인
+const connectedRef = ref(database, '.info/connected');
+onValue(connectedRef, (snapshot) => {
+    if (snapshot.val() === true) {
+        console.log('Firebase에 연결되었습니다.');
+    } else {
+        console.log('Firebase 연결이 끊어졌습니다.');
+    }
+});
+
